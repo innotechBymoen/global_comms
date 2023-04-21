d1bbaf42d09c52c384e3b3092b29edd91850c862
bymoen <alexb@innotechcollege.com>
2023-04-20 18:56:45 -0600

    made global event

---
+++ src/components/SpecialUser.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{ special_username }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                special_username: undefined
+            }
+        },
+        methods: {
+            handle_user_clicked: function(username) {
+                this.special_username = username;
+            }
+        },
+        mounted () {
+            this.$root.$on(`user_clicked`, this.handle_user_clicked);
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/UserList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p v-for="(user, i) in users" :key="i" @click="handle_user" :username="user">Welcome, {{ user }}</p>
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+    export default {
+        methods: {
+            handle_user(details) {
+                let username_value = details[`target`].getAttribute(`username`);
+                this.$root.$emit(`user_clicked`, username_value);
+            }
+        },
+        data() {
+            return {
+                users: [`User_One`,`User_Two`,`User_Three`]
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
